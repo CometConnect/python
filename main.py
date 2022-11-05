c22e5b87c03698c4b3ab1a3039c60a27edceaddb
FloatingComet62 <shubh2007rai@gmail.com>
2022-11-05 12:48:27 +0530

    Roll of the dice

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-import time
+import random
+
+map = ["e", "df", "def", "acgi", "acegi", "acdfgi"]
 
 
-def forma(tim):
-    minutes = tim // 60
-    seconds = tim % 60
+def convert(char):
+    if char == "a": return 1
+    if char == "b": return 2
+    if char == "c": return 3
+    if char == "d": return 7
+    if char == "e": return 8
+    if char == "f": return 9
+    if char == "g": return 13
+    if char == "h": return 14
+    if char == "i": return 15
 
-    return str(minutes)+":"+str(seconds)
+
+def forma(roll):
+    mapped = map[roll-1]
+    output = list("[---]\n[   ]\n[   ]\n[   ]\n[---]")
+    for place in mapped:
+        output[convert(place)+6] = "0"
+
+    return "".join(output)
 
 
-sec = int(input("Enter the time in number of seconds: "))
-time_remaining = sec
-while time_remaining > 0:
-    print(forma(time_remaining), end="\r")
-    time.sleep(1)
-    time_remaining -= 1
-
-print("Times up!")
+running = True
+while running:
+    ran = random.randint(1, 6)
+    print(forma(ran))
+    res = input("press y to roll again and n to exit:")
+    if res == "n":
+        running = False
