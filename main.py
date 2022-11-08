4920d2056231bb2b81aacc7ddc8d5a2c4b720eb9
FloatingComet62 <shubh2007rai@gmail.com>
2022-11-08 16:29:45 +0530

    Event Tracker

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-import shutil
 import os
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Handler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"\n{event.src_path} has been created")
+
+    def on_deleted(self, event):
+        print(f"\n{event.src_path} has been deleted")
+
+    def on_moved(self, event):
+        print(f"\n{event.src_path} has been moved")
+
+    def on_modified(self, event):
+        print(f"\n{event.src_path} has been modified")
+
+
+event_handler = Handler()
+observer = Observer()
+observer.schedule(
+    event_handler,
+    os.getcwd()
+)
+observer.start()
 
 try:
-    shutil.move(
-        os.path.join(os.getcwd(), "Downloads", "l.txt"),
-        os.path.join(os.getcwd(), "Data")
-    )
-except shutil.Error:
-    print("Same file already exists")
+    while True:
+        time.sleep(2)
+        print(".", end="")
+except KeyboardInterrupt:
+    print("!")
+    observer.stop()
