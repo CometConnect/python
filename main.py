a1aa4c710b6e4d8a09d30c56abdc1809455a6d9b
FloatingComet62 <shubh2007rai@gmail.com>
2022-11-20 20:18:09 +0530

    CCTV

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import cv2
-import os
 
-images = os.listdir("Images")
-length = len(images)
-
-i = 0
+classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+cap = cv2.VideoCapture('walking.avi')
 
 while True:
-    data = cv2.imread(f"Images/{images[i]}")
+    ret, frame = cap.read()
 
-    cv2.imshow("Album", data)
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    except:
+        break  # this happens when the video ends
+    faces = classifier.detectMultiScale(gray)
 
-    cv2.waitKey(2000)
-    i += 1
+    for [x, y, width, height] in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
 
-    if i == length:
+    cv2.imshow("Image", frame)
+    if cv2.waitKey(1) == 32:  # Space
         break
+
+cap.release()
+cv2.destroyAllWindows()
