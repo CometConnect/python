d9bb6117963fad64472f53e1441bae43eb24d762
FloatingComet62 <shubh2007rai@gmail.com>
2022-12-04 16:45:09 +0530

    Youtube Vid Controller

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+from pynput.keyboard import Controller, Key
 
 utils = mp.solutions.drawing_utils
 styles = mp.solutions.drawing_styles
@@ -9,6 +10,8 @@ tops = [4, 8, 12, 16, 20]
 bottoms = [2, 6, 10, 14, 18]
 
 vid = cv2.VideoCapture(0)
+controller = Controller()
+prev = 0
 
 with hands.Hands(
         model_complexity=0,
@@ -18,6 +21,7 @@ with hands.Hands(
     while vid.isOpened():
         _, frame = vid.read()
         fingers = 0
+        direction = "null"
 
         frame.flags.writeable = False
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -31,6 +35,10 @@ with hands.Hands(
             continue
 
         hlms = raw[0].landmark
+        if hlms[0].x < hlms[8].x:
+            direction = "right"
+        else:
+            direction = "left"
         for i in range(5):
             top = tops[i]
             bottom = bottoms[i]
@@ -39,19 +47,21 @@ with hands.Hands(
             bottomlm = hlms[bottom]
 
             if not i == 0:
-                if toplm.y < bottomlm.y:  # the y-axis of flipped
+                if toplm.y < bottomlm.y:
                     fingers += 1
-            else:
-                if toplm.x > bottomlm.x: # the thumb
-                    fingers += 1
-
         for hlm in raw:
             utils.draw_landmarks(frame, hlm, None, styles.get_default_hand_landmarks_style(),
                                  styles.get_default_hand_connections_style())
 
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
         frame = cv2.flip(frame, 1)
-        cv2.putText(frame, f"Fingers: {fingers}", (100, 100), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-        cv2.imshow("Image", frame)
+
+        if fingers == 0 and not prev == 0:
+            controller.press(Key.space)
+        elif fingers == 1 and direction == "left":
+            controller.press(Key.right)  # THE FRAME IS FLIPPED WHILE CHECKING
+        elif fingers == 1 and direction == "right":
+            controller.press(Key.left)
+
         cv2.waitKey(5)
+        prev = fingers
