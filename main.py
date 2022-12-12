9248d1d470129e05962ecd01864e1758c1bc5b79
FloatingComet62 <shubh2007rai@gmail.com>
2022-12-12 12:18:33 +0530

    Rock Paper Scissors [NOT WORKING]

--- main.py
+++ main.py
@@ -1,58 +1,41 @@
-import numpy as np
-import pyautogui
-import imutils
 import cv2
-import mediapipe as mp
+import numpy
+from keras.models import Sequential
+from enum import Enum
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-mp_draw = mp.solutions.drawing_utils
-cap = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)
+model = Sequential()
 
-delay = 20
-last_click = 0
-clicks = 0
 
-finger_tips = [8, 12, 16, 20]
-thumb_tip = 4
+class Gesture(Enum):
+    Scissors = 0
+    Paper = 1
+    Rock = 2
+
+
+def get_gesture(image) -> tuple[Gesture, float]:
+    resized = numpy.resize(image, (len(image[0]), len(image)))
+    normalized = resized/numpy.linalg.norm(resized, 1)
+
+    predictions = model.predict(normalized)
+    print(predictions[0][0], " Scissors")
+    print(predictions[0][1], " Paper")
+    print(predictions[0][2], " Rock")
+
+    return Gesture(0), float(predictions[0][0])
+
 
 while True:
-    last_click += 1
-    ret, img = cap.read()
-    img = cv2.flip(img, 1)
-    h, w, c = img.shape
-    results = hands.process(img)
+    status, frame = vid.read()
+    if status:
+        frame = cv2.flip(frame, 1)
+        gesture, score = get_gesture(frame)
 
-    if results.multi_hand_landmarks:
-        for hand_landmark in results.multi_hand_landmarks:
-            # accessing the landmarks by their position
-            lm_list = []
-            for _, lm in enumerate(hand_landmark.landmark):
-                lm_list.append(lm)
-
-            # array to hold true or false if finger is folded
-            finger_fold_status = []
-            for tip in finger_tips:
-                x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
-                cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
-                if lm_list[tip].x < lm_list[tip - 3].x:
-                    cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
-                    finger_fold_status.append(True)
-                else:
-                    finger_fold_status.append(False)
-            if all(finger_fold_status) and delay < last_click:
-                print("Clicking")
-                clicks += 1
-                last_click = 0
-                image = pyautogui.screenshot()
-                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-                cv2.imwrite(f"screenshots/{clicks}.png", image)
-                # WRITE THE CODE HERE
-            mp_draw.draw_landmarks(img, hand_landmark,
-                                   mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0, 0, 255), 2, 2),
-                                   mp_draw.DrawingSpec((0, 255, 0), 4, 2)
-                                   )
-
-    cv2.imshow("hand tracking", img)
-    if cv2.waitKey(1) == 32:
+        cv2.putText(frame, f"{gesture.name}: {score}%", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
+        cv2.imshow('Cam', frame)
+        if cv2.waitKey(1) == 32:
+            break
+    else:
         break
+
+vid.release()
