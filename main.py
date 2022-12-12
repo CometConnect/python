a307c7178cc0d2cc2543a8658a553e69be4173b2
FloatingComet62 <shubh2007rai@gmail.com>
2022-12-12 10:27:23 +0530

    Screenshots

--- main.py
+++ main.py
@@ -1,61 +1,58 @@
+import numpy as np
+import pyautogui
+import imutils
 import cv2
-import math
+import mediapipe as mp
 
-p1 = 530
-p2 = 300
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
 
-xs = []
-ys = []
-windowName = "FootVolleyBall"
+delay = 20
+last_click = 0
+clicks = 0
 
-video = cv2.VideoCapture("footvolleyball.mp4")
-tracker = cv2.TrackerCSRT_create()
-
-_, img = video.read()
-tracker.init(img, cv2.selectROI(windowName, img, False))
-
-
-def goal_track(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-    c1 = x + int(w / 2)
-    c2 = y + int(h / 2)
-    cv2.circle(img, (c1, c2), 2, (0, 0, 255), 5)
-
-    cv2.circle(img, (int(p1), int(p2)), 2, (0, 255, 0), 3)
-    dist = math.sqrt(((c1 - p1) ** 2) + (c2 - p2) ** 2)
-
-    if dist <= 20:
-        cv2.putText(img, "Goal", (300, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
-    xs.append(c1)
-    ys.append(c2)
-
-    for i in range(len(xs) - 1):
-        cv2.circle(img, (xs[i], ys[i]), 2, (0, 0, 255), 5)
-
-
-def drawBox(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-    cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
-    cv2.putText(img, "Tracking", (75, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
-
-def updateROI(img):
-    tracker.init(img, cv2.selectROI(windowName, img, False))
-
+finger_tips = [8, 12, 16, 20]
+thumb_tip = 4
 
 while True:
-    try:
-        _, img = video.read()
-        __, bbox = tracker.update(img)
-        if bbox == (0, 0, 0, 0):
-            updateROI(img)
-        drawBox(img, bbox)
-        goal_track(img, bbox)
+    last_click += 1
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    h, w, c = img.shape
+    results = hands.process(img)
 
-        cv2.imshow(windowName, img)
-        if cv2.waitKey(1) == 32:
-            break
-    except cv2.error:
+    if results.multi_hand_landmarks:
+        for hand_landmark in results.multi_hand_landmarks:
+            # accessing the landmarks by their position
+            lm_list = []
+            for _, lm in enumerate(hand_landmark.landmark):
+                lm_list.append(lm)
+
+            # array to hold true or false if finger is folded
+            finger_fold_status = []
+            for tip in finger_tips:
+                x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+                cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+                if lm_list[tip].x < lm_list[tip - 3].x:
+                    cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
+            if all(finger_fold_status) and delay < last_click:
+                print("Clicking")
+                clicks += 1
+                last_click = 0
+                image = pyautogui.screenshot()
+                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+                cv2.imwrite(f"screenshots/{clicks}.png", image)
+                # WRITE THE CODE HERE
+            mp_draw.draw_landmarks(img, hand_landmark,
+                                   mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0, 0, 255), 2, 2),
+                                   mp_draw.DrawingSpec((0, 255, 0), 4, 2)
+                                   )
+
+    cv2.imshow("hand tracking", img)
+    if cv2.waitKey(1) == 32:
         break
-video.release()
