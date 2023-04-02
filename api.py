d5bfabf225b139ca28dcdde16764aa1275d9d44b
FloatingComet62 <shubh2007rai@gmail.com>
2023-04-02 17:32:14 +0530

    prettier

--- api.py
+++ api.py
@@ -4,16 +4,32 @@ from flask import Flask, jsonify
 from pandas import read_csv
 
 app = Flask(__name__)
+data = read_csv("csv/display.csv")
 
 @app.get("/")
 def index():
-  data = read_csv("csv/display.csv")
-  return data.to_json(orient='records')[1:-1].replace('},{', '} {')
+  to_send = []
+  i = 1
+  while True:
+    res = get_data(i)
+    if res == "":
+      break
+    to_send.append(res)
+    i += 1
+  return to_send
+
 
 @app.route("/get/<int:i>")
 def get_data(i):
-  data = read_csv("csv/display.csv")
-  return data.iloc[i].to_json(orient='records')[1:-1].replace('},{', '} {')
+  try:
+    to_send = {}
+    columns = data.columns[1:]
+    response = data.iloc[i].to_json(orient='records')[1:-1].split(",")[1:]
+    for j, item in enumerate(response):
+      to_send.update({ columns[j]: item.replace("\"", "").replace("\"", "") })
+    return to_send
+  except:
+    return ""
 
 
 if __name__ == "__main__":
