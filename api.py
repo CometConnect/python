f3c54bbad5a42387731ed90aa2d8cc5f2327ac60
FloatingComet62 <shubh2007rai@gmail.com>
2023-09-24 13:36:09 +0530

    forgot to add this

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 # Things to show
 # Name, Orbital Radius, Gravity, Mass, Distance, Planet Type, Goldilock, Discovery Date, Mass of hoststar
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from pandas import read_csv
 
 app = Flask(__name__)
+
 data = read_csv("csv/display.csv")
 
 @app.get("/")
@@ -12,23 +13,24 @@ def index():
   i = 1
   while True:
     res = get_data(i)
-    if res == "":
+    if res[0] == False:
       break
-    to_send.append(res)
+    to_send.append(res[1])
     i += 1
-  return to_send
+  return cors(jsonify(to_send))
 
 @app.route("/home")
 def get_home():
   to_send = []
   columns = data.columns[1:]
+  columns_to_share = ["name", "planet_type"]
   i = 0
   while True:
     try:
       planet_data = {}
       response = data.iloc[i].to_json(orient='records')[1:-1].split(",")[1:]
       for j, item in enumerate(response):
-        if columns[j] == "name" or columns[j] == "planet_type":
+        if columns[j] in columns_to_share:
           planet_data.update({ columns[j]: item.replace("\"", "").replace("\"", "") })
       planet_data.update({ "index": i })
       to_send.append(planet_data)
@@ -39,6 +41,9 @@ def get_home():
 
 
 @app.route("/get/<int:i>")
+def get_data_end_point(i):
+  return cors(jsonify(get_data(i)))
+
 def get_data(i):
   try:
     to_send = {}
@@ -46,10 +51,14 @@ def get_data(i):
     response = data.iloc[i].to_json(orient='records')[1:-1].split(",")[1:]
     for j, item in enumerate(response):
       to_send.update({ columns[j]: item.replace("\"", "").replace("\"", "") })
-    return to_send
+    return [True, to_send]
   except:
-    return ""
+    return [False, {}]
 
+
+def cors(res):
+  res.headers.add("Access-Control-Allow-Origin", "*")
+  return res
 
 if __name__ == "__main__":
   app.run()
